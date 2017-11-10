4f10804b3c81a08a667d1c563e21eb848770e6b8
OmEgA357 <vaibhav.sawant@ves.ac.in>
2017-11-10 16:12:30 +0530

    Add files via upload

--- RunningClass_Final.java
+++ RunningClass_Final.java
@@ -33,7 +33,7 @@ public class RunningClass {
 
 	public static final String JMX_PORT = "spring.boot.livebean.port";
 
-	public static final int DEFAULT_JMX_PORT = 0; //means pick it dynamically
+	public static final int DEFAULT_JMX_PORT = 0; // means pick it dynamically
 
 	public static final String ANSI_CONSOLE_OUTPUT = "spring.boot.ansi.console";
 
@@ -47,47 +47,49 @@ public class RunningClass {
 
 	private static final String TERMINATION_TIMEOUT = "spring.boot.lifecycle.termination.timeout";
 	public static final long DEFAULT_TERMINATION_TIMEOUT = 15000; // 15 seconds
-	
+
 	public void runProject() throws CoreException {
-		
+
 		ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
 
 		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 		IProject[] iProjects = workspaceRoot.getProjects();
-		
+
 		IProject selectedProject = null;
 		IJavaProject selectedJavaProject = null;
 		IJavaElement javaElement = null;
-		
-        for(IProject iProject: iProjects) {
-            if(iProject.getName().equalsIgnoreCase("EmployeeDemo-microservice-module")) {
-            	selectedProject = iProject;
-            	break;
-            }
-         }
-        
-        if ( selectedProject != null ){ 
-	        if ( selectedProject instanceof IAdaptable ){ 
-	        	IAdaptable a = ( IAdaptable ) selectedProject;
-	        	
-	        	javaElement = (IJavaElement) a.getAdapter(IJavaElement.class); 
-	        	selectedJavaProject = javaElement.getJavaProject(); 
-	        } 
-        }
-        
+
+		for (IProject iProject : iProjects) {
+			if (iProject.getName().equalsIgnoreCase("EmployeeDemo-microservice-module")) {
+				selectedProject = iProject;
+				break;
+			}
+		}
+
+		if (selectedProject != null) {
+			if (selectedProject instanceof IAdaptable) {
+				IAdaptable a = (IAdaptable) selectedProject;
+
+				javaElement = (IJavaElement) a.getAdapter(IJavaElement.class);
+				selectedJavaProject = javaElement.getJavaProject();
+			}
+		}
+
 		IType mainType = MainTypeFinder.guessMainTypes(selectedJavaProject, new NullProgressMonitor())[0];
-		
-		ILaunchConfigurationType configType = manager.getLaunchConfigurationType("org.springframework.ide.eclipse.boot.launch");
-		ILaunchConfigurationWorkingCopy workingCopy = configType.newInstance(null, manager.generateLaunchConfigurationName(
-				selectedProject+"-runconfig"));
-		
+
+		ILaunchConfigurationType configType = manager
+				.getLaunchConfigurationType("org.springframework.ide.eclipse.boot.launch");
+		ILaunchConfigurationWorkingCopy workingCopy = configType.newInstance(null,
+				manager.generateLaunchConfigurationName(selectedProject + "-runconfig"));
+
 		workingCopy.setAttribute(ATTR_PROJECT_NAME, selectedProject.getName());
-		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, mainType.getFullyQualifiedName());
+		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME,
+				mainType.getFullyQualifiedName());
 		workingCopy.setAttribute(ENABLE_JMX, DEFAULT_ENABLE_JMX);
 		workingCopy.setAttribute(ENABLE_LIVE_BEAN_SUPPORT, DEFAULT_ENABLE_LIVE_BEAN_SUPPORT);
 		workingCopy.setAttribute(ENABLE_LIFE_CYCLE, DEFAULT_ENABLE_LIFE_CYCLE);
-		workingCopy.setAttribute(TERMINATION_TIMEOUT, ""+DEFAULT_TERMINATION_TIMEOUT);
-		
+		workingCopy.setAttribute(TERMINATION_TIMEOUT, "" + DEFAULT_TERMINATION_TIMEOUT);
+
 		ILaunchConfiguration configuration = workingCopy.doSave();
 		DebugUITools.launch(configuration, ILaunchManager.RUN_MODE);
 	}
